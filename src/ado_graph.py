5158a0a00340e6c38d492139aae8228ee7df9dfa
Tyler Yep <tyep@stanford.edu>
2020-06-01 21:59:01 -0700

    add extra documentation

--- src/ado_graph.py
+++ src/ado_graph.py
@@ -27,30 +27,30 @@ class ApproxDistanceOracle:
     def __init__(self, V: Set[Vertex], E: List[List[Vertex]], k=2):
         """
         Preprocessing step.
-        Initialize k+1 sets of vertices with decreasing sizes. (i-centers)
+
+        Note that
+            self.p represents witnesses
+            self.a_i_v_distances represents delta(A_i, v)
+
         """
         self.E = E
         self.V = V
         self.n = len(V)  # number of vertices
 
-        self.delta: Dict[Vertex, List[int]] = {}
-        self.p: Dict[int, List[int]] = {}
+        # Initialize k+1 sets of vertices with decreasing sizes. (i-centers)
         self.A: List[Set[Vertex]] = [None for _ in range(k + 1)]
-        self.a_i_v_distances: List[int, Dict[Vertex, int]] = [None for _ in range(k+1)]
-
-        # Construct the A_i samples
         self.A[0] = V
         self.A[k] = set()
         for i in range(1, k):  # for i = 1 to k - 1
             prob = self.n ** (-1 / k)
             self.A[i] = {x for x in self.A[i - 1] if weighted_coin_flip(prob)}
 
+        self.a_i_v_distances: List[int, Dict[Vertex, int]] = [None for _ in range(k+1)]
+        self.p: Dict[int, List[int]] = {}
+
         # Initialize a_i_v_distances of A_k to INF
         self.a_i_v_distances[k] = {v: INF for v in self.V}
         self.p[k] = None
-
-        print(f"V: {self.V}")
-        print(f"E: {self.E}")
 
         self.C: Dict[Vertex, Set[Vertex]] = {}
 
@@ -69,6 +69,8 @@ class ApproxDistanceOracle:
                     if self.a_i_v_distances[i][v] == self.a_i_v_distances[i + 1][v]:
                         self.p[i] = self.p[i + 1]
 
+            # TODO: probably something missing here - I followed the pseudocode but they
+            # also mention creating a shortest path tree T(w) and then don't mention it again...?
             for w in self.A[i] - self.A[i + 1]:
                 self.C[w] = {
                     v
@@ -76,6 +78,8 @@ class ApproxDistanceOracle:
                     if v != s and self.distance_fn(w, v)[0] < self.a_i_v_distances[i+1][v]
                 }
 
+        # TODO: This rest of this function is just copy pasted from the finite metric version.
+        # There are probably changes that need to happen.
         self.B: Dict[Vertex, Set[Vertex]] = {}
         for v in self.V:
             self.B[v] = set()
@@ -89,6 +93,8 @@ class ApproxDistanceOracle:
 
 
     def query(self, u, v):
+        # TODO: This function is just copy pasted from the finite metric version.
+        # Make changes if necessary.
         w = u
         i = 0
         while w not in self.B[v]:
@@ -98,9 +104,11 @@ class ApproxDistanceOracle:
         return self.hash_table[(w, u)] + self.hash_table[(w, v)]
 
 
-    # Adds a source vertex that is connected with edge weight 0 to all
-    # nodes in A_i. Returns the index of the added vertex
     def add_source(self, i):
+        """
+        Adds a source vertex that is connected with edge weight 0 to all
+        nodes in A_i. Returns the index of the added vertex
+        """
         self.E.append([0 if v in self.A[i] else INF for v in self.V])
         for v in self.A[i]:
             self.E[v].append(0)
@@ -110,8 +118,10 @@ class ApproxDistanceOracle:
         return s
 
 
-    # Variant on Dijkstra's trying to also keep track of witnesses
     def distance_fn(self, src, dst=None):
+        """
+        Variant on Dijkstra's trying to also keep track of witnesses
+        """
         q = [(0, (src, None))]
         distances = [0 if v == src else INF for v in self.V]
         witnesses = [INF for v in self.V]
@@ -141,28 +151,36 @@ def weighted_coin_flip(prob):
     return random.choices([True, False], [prob, 1 - prob])[0]
 
 
-# Returns index of minimum element in array a
 def argmin(a):
+    """
+    Returns index of minimum element in array a
+    """
     if not a:
         return None
     return min(range(len(a)), key=lambda x: a[x])
 
 
 if __name__ == "__main__":
+    # Fix seed to preserve sanity
     random.seed(0)
+
+
     V: Set[Vertex] = set([i for i in range(4)])
     # INF means there is no edge between the vertices
     E: List[List[Vertex]] = [
-        [1, 2, 3, 4],
-        [2, 2, 5, 6],
-        [3, 5, 7, 1],
-        [4, 6, 1, 9],
+        # [1, 2, 3, 4],
+        # [2, 2, 5, 6],
+        # [3, 5, 7, 1],
+        # [4, 6, 1, 9],
 
-        # [0, 1, 1, INF],
-        # [1, 0, 1, INF],
-        # [1, 1, 0, 1],
-        # [INF, INF, 1, 0],
+        [0, 1, 1, INF],
+        [1, 0, 1, INF],
+        [1, 1, 0, 1],
+        [INF, INF, 1, 0],
     ]
+
+    print(f"V: {V}")
+    print(f"E: {E}")
 
     ado = ApproxDistanceOracle(V, E)
     x = ado.query(0, 2)
